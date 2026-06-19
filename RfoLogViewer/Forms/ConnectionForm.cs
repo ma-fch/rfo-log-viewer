e6ef681a561ccf2e61bad3ee849c45823eb0471c
ma-fch <fabien.chabod@regnology.net>
2026-06-19 15:17:45 +0200

    Add a decent icon to make the application look more serious

--- RfoLogViewer/Forms/ConnectionForm.cs
+++ RfoLogViewer/Forms/ConnectionForm.cs
@@ -24,6 +24,7 @@ namespace RfoLogViewer.Forms
         public ConnectionForm()
         {
             this.Text = "RFo Log Viewer - Connection";
+            this.Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath);
             this.FormBorderStyle = FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
